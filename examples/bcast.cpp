5a0247c83853f844edd92605b478735e9956bee6
Roman Iakymchuk <roman.iakymchuk@itwm.fraunhofer.de>
2019-06-06 11:11:06 +0200

    first implementation of eventually consistent gaspi bcast

--- examples/bcast.cpp
+++ examples/bcast.cpp
@@ -6,11 +6,52 @@
 #include "success_or_die.h"
 #include "queue.h"
 
-int main( )
-{
+// testing regular gaspi bcast
+void test_bcast(){
   static const int VLEN = 8;
   
-  SUCCESS_OR_DIE( gaspi_proc_init(GASPI_BLOCK) );
+  gaspi_rank_t iProc, nProc, root = 0;
+  SUCCESS_OR_DIE( gaspi_proc_rank(&iProc) );
+  SUCCESS_OR_DIE( gaspi_proc_num(&nProc) );
+
+  gaspi_segment_id_t const segment_id = 0;
+  gaspi_size_t       const segment_size = VLEN * sizeof (double);
+
+  SUCCESS_OR_DIE
+    ( gaspi_segment_create
+      ( segment_id, segment_size
+      , GASPI_GROUP_ALL, GASPI_BLOCK, GASPI_MEM_INITIALIZED
+      )
+    );
+    
+  gaspi_pointer_t array;
+  SUCCESS_OR_DIE( gaspi_segment_ptr (segment_id, &array) );
+ 
+  double * src_array = (double *)(array);
+
+  for (int j = 0; j < VLEN; ++j)
+  {
+      if (iProc == root) 
+          src_array[j]= (double)( iProc * VLEN + j );
+  }
+
+  gaspi_queue_id_t queue_id = 0;
+
+  gaspi_bcast(segment_id, 0, VLEN, GASPI_TYPE_DOUBLE, root, queue_id);
+
+  for (int j = 0; j < VLEN; ++j)
+  {
+      printf("rank %d rcv elem %d: %f \n", iProc, j, src_array[j] );
+  }
+  printf("\n");
+  
+  wait_for_flush_queues();
+}
+
+
+// testing evntually consistent gaspi bcast
+void test_consist_bcast(){
+  static const int VLEN = 8;
  
   gaspi_rank_t iProc, nProc, root = 0;
   SUCCESS_OR_DIE( gaspi_proc_rank(&iProc) );
@@ -32,13 +73,15 @@ int main( )
   double * src_array = (double *)(array);
 
   for (int j = 0; j < VLEN; ++j)
-    {
+  {
       if (iProc == root) 
           src_array[j]= (double)( iProc * VLEN + j );
-    }
+  }
 
   gaspi_queue_id_t queue_id = 0;
-  gaspi_bcast(segment_id, 0, VLEN, GASPI_TYPE_DOUBLE, root, queue_id);
+
+  gaspi_double threshold = 0.6;
+  gaspi_bcast(segment_id, 0, VLEN, GASPI_TYPE_DOUBLE, threshold, root, queue_id);
 
   for (int j = 0; j < VLEN; ++j)
   {
@@ -47,8 +90,19 @@ int main( )
   printf("\n");
   
   wait_for_flush_queues();
+}
+
+
+int main( )
+{
+  
+  SUCCESS_OR_DIE( gaspi_proc_init(GASPI_BLOCK) );
+
+  test_consist_bcast(); 
+
+  test_bcast(); 
  
   SUCCESS_OR_DIE( gaspi_proc_term(GASPI_BLOCK) );
- 
+
   return EXIT_SUCCESS;
 }
--- src/consistent.collectives.cxx
+++ src/consistent.collectives.cxx
@@ -1,4 +1,5 @@
 
+#include <cmath>
 
 #include <consistent.collectives.hxx>
 
@@ -38,6 +39,52 @@ gaspi_bcast (gaspi_segment_id_t const buf,
 			    ( gaspi_write_notify
 			      ( buf, offset*type_size, k
 			      , buf, offset*type_size, elem_cnt*type_size
+			      , data_available, root+1 // +1 so that the value is not zero
+			      , queue_id, GASPI_BLOCK
+			      )
+			    );
+
+	    }
+    } else {
+  	wait_or_die ( buf, data_available, root+1 );  
+    }
+
+    return;
+}
+
+void
+gaspi_bcast (gaspi_segment_id_t const buf,
+	     gaspi_number_t const offset,
+	     gaspi_number_t const elem_cnt,
+	     const gaspi_datatype_t type,
+	     const gaspi_double threshold,
+	     const gaspi_number_t root,
+             const gaspi_queue_id_t queue_id)
+{
+
+    gaspi_rank_t iProc, nProc; 
+    SUCCESS_OR_DIE( gaspi_proc_rank(&iProc) );
+    SUCCESS_OR_DIE( gaspi_proc_num(&nProc) );
+
+    gaspi_notification_id_t data_available = 0;
+
+    // get size of type, see GASPI.h for details
+    gaspi_number_t type_size = 0;
+    if (type >= 3) 
+	type_size = 8;
+    else
+	type_size = 4;
+ 
+    if (iProc == root) {	
+	    for(uint k = 0; k < nProc; k++) {
+		if (k == root) 
+			continue;
+
+		//SUCCESS_OR_DIE(gaspi_write(buf, offset, k, buf, offset, size, 0, GASPI_BLOCK));
+		SUCCESS_OR_DIE
+			    ( gaspi_write_notify
+			      ( buf, offset*type_size, k
+			      , buf, offset*type_size, ceil(elem_cnt * threshold)*type_size
 			      , data_available, root+1 // +1 so that the value is not zero
 			      , queue_id, GASPI_BLOCK
 			      )
